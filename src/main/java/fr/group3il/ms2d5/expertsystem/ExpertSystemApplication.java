d333f1c47e44e56d9687d67fe70d267ef746cdc5
Evrad7 <tsoataevrad7@gmail.com>
2024-10-22 09:19:00 +0200

    initialisation projet: ajout des bibliothèques et configurations

---
+++ src/main/java/fr/group3il/ms2d5/expertsystem/ExpertSystemApplication.java
@@ -0,0 +1,13 @@
+package fr.group3il.ms2d5.expertsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpertSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/group3il/ms2d5/expertsystem/ExpertSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.group3il.ms2d5.expertsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
